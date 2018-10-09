6512332ab21fc86a38a4458e473f492eeda8f873
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-08 22:15:43 -0500

    add materialize components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import LocationList from './components/Weather/LocationList/LocationList';
 
 import './App.css';
@@ -18,11 +26,35 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <LocationList
-          cities={cities}
-          onSelectedLocation= { this.handleSelectionLocation } />
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <AppBar>
+              <Toolbar variant="dense">
+                <IconButton color="inherit" aria-label="Menu">
+                  <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" color="inherit">
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={7}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation= { this.handleSelectionLocation } />
+          </Col>
+          <Col xs={12} md={5}>
+            <Paper elevation={5}>
+              <div className='detail'>
+              </div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
