955ee14f0c775abb2479e6e77f705a6c1827f1fb
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-18 21:31:42 -0500

    Updates

---
+++ precache-manifest.d61be4842b5f8b2c5345f6282a1574d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57b2d9b5fe565515522",
+    "url": "/weather-react/static/js/runtime~main.e57b2d9b.js"
+  },
+  {
+    "revision": "a82b1733c4a803d611de",
+    "url": "/weather-react/static/js/main.a82b1733.chunk.js"
+  },
+  {
+    "revision": "0c9ef6a67df2789324d6",
+    "url": "/weather-react/static/js/1.0c9ef6a6.chunk.js"
+  },
+  {
+    "revision": "a82b1733c4a803d611de",
+    "url": "/weather-react/static/css/main.75579c07.chunk.css"
+  },
+  {
+    "revision": "0c9ef6a67df2789324d6",
+    "url": "/weather-react/static/css/1.ee29a482.chunk.css"
+  },
+  {
+    "revision": "7946ecef4bc1354bde23429c89145d5e",
+    "url": "/weather-react/index.html"
+  }
+];
